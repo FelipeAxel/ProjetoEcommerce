eeca9b277b3238911c40b454b9d40f1eedc23648
FelipeAxel <axelnovais96@gmail.com>
2022-04-26 18:04:18 -0300

    Inicialização do projeto ecommerce

---
+++ src/main/java/com/axelfelipe/curso/nelioalves/AxelnovaisApplication.java
@@ -0,0 +1,13 @@
+package com.axelfelipe.curso.nelioalves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxelnovaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxelnovaisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/axelfelipe/curso/nelioalves/AxelnovaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axelfelipe.curso.nelioalves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxelnovaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

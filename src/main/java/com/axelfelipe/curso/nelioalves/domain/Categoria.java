b4474ca9807e3cf5ba46ee0b2326d78fa4ab1996
FelipeAxel <axelnovais96@gmail.com>
2022-04-26 22:08:05 -0300

    Banco de dados H2, criação automatica da tabela categoria

--- src/main/java/com/axelfelipe/curso/nelioalves/domain/Categoria.java
+++ src/main/java/com/axelfelipe/curso/nelioalves/domain/Categoria.java
@@ -2,18 +2,28 @@ package com.axelfelipe.curso.nelioalves.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
 @Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class Categoria implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
